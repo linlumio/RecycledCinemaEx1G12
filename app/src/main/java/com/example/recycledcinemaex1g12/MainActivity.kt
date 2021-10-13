12f9c50724cba201e6a1bf2e6d4d7b65e3affe30
Ale <akip-core@outlook.com>
2021-10-13 17:42:03 +0200

    manifest per recycled

--- app/src/main/java/com/example/recycledcinemaex1g12/MainActivity.kt
+++ app/src/main/java/com/example/recycledcinemaex1g12/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.recycledcinemaex1g12
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recycledcinemaex1g12.adapter.RecycledAdapter
 
 class MainActivity : AppCompatActivity() {
+    lateinit var recycler:RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //richiesta al server
+
+        //salvataggio dati
+
+        //preparazione lista
+
+        //lista in view
+        recycler = findViewById(R.id.recycled_view)
+        recycler.layoutManager = LinearLayoutManager(this)
+        recycler.adapter = RecycledAdapter(listona, this)
     }
 }
